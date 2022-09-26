8c13266716a8076b42fbe8899cf468d88068fabc
haiou <3529591183@qq.com>
2022-09-26 09:23:43 +0800

    登录完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,4 +6,19 @@ const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout:5000
 })
+service.interceptors.response.use(response => {
+  // 1. 需要哪些数据
+  // 2. 接口成功并且业务成功
+  // 3. 没有成功Promise.reject就会报错
+  const { message, data, success } = response.data
+  if (success) { // 业务逻辑成功的时候返回data
+    return data
+  }
+  // 接口没有成功的时候 
+  Message.error(message)
+  return Promise.reject(new Error(message))
+}, error => {
+  Message.error(error.message)
+  return Promise.reject(reeor)
+})
 export default service
