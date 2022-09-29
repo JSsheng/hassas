1ffd921cbec2c26b978e1c3661dbe3ff4f5b242c
haiou <3529591183@qq.com>
2022-09-29 16:31:51 +0800

    feta:组织架构完成一半

---
+++ src/views/departments/componets/terr-tools.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <el-row type="flex" justify="space-between" align="middle" style="width: 100%">
+      <el-col>
+        <span>{{ treeNode.name }}</span>
+      </el-col>
+      <el-col :span="4">
+        <el-row type="flex" justify="right">
+          <span style="margin-right:8px;">{{ treeNode.manager }}</span>
+          <el-dropdown>
+            <span>操作 <i class="el-icon-arrow-down el-icon--right" /> </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="isRoot">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="isRoot">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HrsaasTreeTools',
+  props: {
+    treeNode: {
+      type: Object,
+      required: true
+    },
+    isRoot: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,29 +1,56 @@
 <template>
-  <div>
-    departments
+  <div class="departments-container">
+    <el-card>
+      <treeTools :tree-node="company" :is-root="false" />
+    </el-card>
+    <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+      <treeTools slot-scope="{data}" :tree-node="data" />
+    </el-tree>
   </div>
 </template>
 
 <script>
+import { getDepartments } from '@/api/departments'
+import treeTools from '@/views/departments/componets/terr-tools.vue'
 export default {
   name: 'HrsaasIndex',
+  components: {
+    treeTools
+  },
 
   data() {
     return {
-
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
+      departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }],
+      defaultProps: {
+        label: 'name' // 表示 从这个属性显示内容
+      }
     }
   },
 
   mounted() {
-
+    this.getDepartments()
   },
 
   methods: {
-
+    async getDepartments() {
+      const res = await getDepartments()
+      console.log(res)
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+.departments-container {
+  width: 900px;
+  margin: 20px auto;
+  .el-tree {
+    .el-tree-node__content {
+      padding-right: 20px;
+    }
+  }
+}
 </style>
